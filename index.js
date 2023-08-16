66874391c4a0bdeb64f5443c095203f4593cc9b5
myahyaibrahim <yahyaibrahim01@gmail.com>
2023-08-16 13:44:57 +0700

    add dockerfile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 300;
+const port = process.env.PORT || 8080;
 const cors = require("cors");
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
